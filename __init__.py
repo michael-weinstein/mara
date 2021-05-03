d14b407a1fb83c3a056b3fea6595b45e31e1901a
Michael Weinstein <michael.weinstein@ucla.edu>
2021-05-02 23:10:51 -0700

    Basic functionality tested and working at least against D614G and T716I from the spike

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from maraSupport.altSequence import getAltCodons
+import maraSupport
+
+__all__ = ["maraSupport"]
--- main.py
+++ main.py
@@ -2,35 +2,46 @@
 import maraSupport
 import sys
 import typing
+import json
 
 
 def getMutationFromArguments():
     args = sys.argv
-    if not len(args) == 3:
+    if not len(args) == 2:
         raise ValueError("This program takes only 1 argument: the mutation being analyzed")
-    mutation = args[2]
+    mutation = args[1]
+    return mutation
 
 
 def analyzeMutation(mutation:str):
     if not maraSupport.altSequence.validateMutation(mutation):
         raise ValueError("Unable to validate the mutation as readable")
     mostLikelyMutations, otherPossibleMutations = maraSupport.altSequence.getAltCodons(mutation)
+    return mostLikelyMutations, otherPossibleMutations
 
 
-def printMutationResults(codonList:typing.List[maraSupport.altSequence.PotentialAlternativeSequence]):
-    for possibleMutation in codonList:
-        print(possibleMutation.json)
+def getResultDict(mostLikelyCodons:typing.List[maraSupport.altSequence.PotentialAlternativeSequence], otherPossibleCodons:typing.Dict[int, typing.List[maraSupport.altSequence.PotentialAlternativeSequence]]):
+    resultDict = {
+        "best": [],
+        "other": {}
+    }
+    for potentialCodon in mostLikelyCodons:
+        resultDict["best"].append(potentialCodon.dictionary)
+    for changeCount, potentialCodonList in otherPossibleCodons.items():
+        resultDict["other"][changeCount] = [potentialCodon.dictionary for potentialCodon in potentialCodonList]
+    return resultDict
 
 
-def printResults(mostLikelyCodons:list, otherPossibleCodons:list):
-    print("MOST LIKELY CHANGES")
-    printMutationResults(mostLikelyCodons)
-    print()
-    print("OTHER POSSIBLE CHANGES")
-    printMutationResults(otherPossibleCodons)
+def printResultDict(resultDict):
+    print(json.dumps(resultDict, indent=2))
 
 
 def main():
     mutation = getMutationFromArguments()
     mostLikelyMutations, otherPossibleMutations = analyzeMutation(mutation)
-    printResults(mostLikelyMutations, otherPossibleMutations)
+    resultDict = getResultDict(mostLikelyMutations, otherPossibleMutations)
+    printResultDict(resultDict)
+
+
+if __name__ == "__main__":
+    main()
